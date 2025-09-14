51281eb07eefb53d7cac52c8c15a86cc1acf36ef
LukasOmniDock <lukas.koch@omnidock.com>
2025-09-15 00:33:00 +0200

    call credit update

--- frontend/src/lib/api.profile.ts
+++ frontend/src/lib/api.profile.ts
@@ -10,3 +10,12 @@ export async function getCredits(): Promise<any> {
   const res = await apiFetch("/profile/get-credits");
   return res.json();
 }
+
+
+export async function updateCredits(prank_credit_amount: number, call_credit_amount: number): Promise<any> {
+  const res = await apiFetch("/profile/update-credits", {
+    method: "POST",
+    body: JSON.stringify({ 'prank_credit_amount': prank_credit_amount, 'call_credit_amount': call_credit_amount }),
+  });
+  return res.json();
+}
